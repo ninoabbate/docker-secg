4fc3d5ba0ee0ea5e9ebff9265dabedc2548559c3
Antonino Abbate <aabbate@vendini.com>
2017-07-26 16:13:06 +0200

    Updated script and dockerfile to match and use snmp_exporter release tags

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM alpine:3.5
 
 MAINTAINER Antonino Abbate "ninoabbate@gmail.com"
 
+# NOTE: this ENV is populated when running the build script
+# if you need to build manually the container just populate it with the release tag
+ENV APP_VERSION
+
 RUN apk --update add go git net-snmp net-snmp-tools net-snmp-dev g++
 
 # snmp exporter config generator
@@ -11,7 +15,9 @@ RUN mkdir -p $HOME/.snmp/mibs \
 	&& git clone --branch v2 https://github.com/go-yaml/yaml $GOPATH/src/gopkg.in/yaml.v2 \
 	&& cp /usr/lib/libnetsnmp.so.30 /usr/lib/libsnmp.so \
 	&& git clone https://github.com/prometheus/snmp_exporter.git \
-	&& cd /snmp_exporter/generator \
+	&& cd /snmp_exporter \
+	&& git checkout tags/${APP_VERSION} \
+	&& cd generator \
 	&& go get -d \
 	&& go build
 
--- build.sh
+++ build.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-# Set the tag with current date
-TAG=$(date +"%Y.%m.%d-%H.%M.%S")
+# Get the latest release tag
+TAG=$(curl https://api.github.com/repos/prometheus/snmp_exporter/releases/latest -s | jq .tag_name -r)
+
+# Set the version in Dockerfile before building the container
+OS=$(uname -a | awk '{print $1}')
+if [ $OS = "Darwin" ]; then
+    # If the operating system where this script runs is OSX
+    sed -i'' -e 's/ENV APP_VERSION/ENV APP_VERSION '${TAG}'/' ${PWD}/Dockerfile
+    rm -rf ${PWD}/Dockerfile-e
+else
+    sed -i 's/ENV APP_VERSION/ENV APP_VERSION '${TAG}'/' ${PWD}/Dockerfile
+fi
 
 # Build the image
 docker build --label=secg -t aabbate/secg:${TAG} -t aabbate/secg:latest .
 
-# Push the container to Docker Hub
+# Push the container to DockerHub
 docker commit -p secg aabbate/secg:${TAG}
 docker push aabbate/secg:${TAG}
 docker commit -p secg aabbate/secg:latest
 docker push aabbate/secg:latest
+
+# Remove the tag from Dockerfile
+if [ $OS = "Darwin" ]; then
+    # If the operating system where this script runs is OSX
+    sed -i'' -e 's/ENV APP_VERSION '${TAG}'/ENV APP_VERSION/' ${PWD}/Dockerfile
+    rm -rf ${PWD}/Dockerfile-e
+else
+    sed -i 's/ENV APP_VERSION '${TAG}'/ENV APP_VERSION/' ${PWD}/Dockerfile
+fi
